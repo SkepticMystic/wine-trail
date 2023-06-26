5d9ab62480500f1a8a3c0bcadd0564dfd9182d26
SkepticMystic <rossk29@gmail.com>
2023-06-26 09:00:21 +0200

    Fancier carousel

--- src/lib/components/ImageCarousel.svelte
+++ src/lib/components/ImageCarousel.svelte
@@ -2,10 +2,25 @@
   export let images: string[];
 </script>
 
-<div class="carousel rounded-box">
-  {#each images as src}
-    <div class="carousel-item">
-      <img width={300} {src} />
+<div class="carousel w-full max-w-sm">
+  {#each images as src, i}
+    {@const [prev, next] = [i - 1, i + 1]}
+    <div id="slide{i}" class="carousel-item relative w-full">
+      <img class="w-full" {src} />
+
+      <div
+        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+      >
+        <a href="#slide{prev}">
+          <button class="btn btn-circle" disabled={prev < 0}> ❮ </button>
+        </a>
+
+        <a href="#slide{next}">
+          <button class="btn btn-circle" disabled={next >= images.length}>
+            ❯
+          </button>
+        </a>
+      </div>
     </div>
   {/each}
 </div>
