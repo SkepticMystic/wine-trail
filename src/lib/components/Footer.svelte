274e4efa43cb551329a268898747078a9a740de2
SkepticMystic <rossk29@gmail.com>
2023-06-26 11:44:09 +0200

    Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,26 @@
+<footer class="footer p-10 bg-base-200 text-base-content">
+  <div>
+    <p class="text-3xl">☯️</p>
+    <p class="text-xl">Yoga List</p>
+  </div>
+  <div>
+    <span class="footer-title">Services</span>
+    <a class="link link-hover">Branding</a>
+    <a class="link link-hover">Design</a>
+    <a class="link link-hover">Marketing</a>
+    <a class="link link-hover">Advertisement</a>
+  </div>
+  <div>
+    <span class="footer-title">Company</span>
+    <a class="link link-hover">About us</a>
+    <a class="link link-hover">Contact</a>
+    <a class="link link-hover">Jobs</a>
+    <a class="link link-hover">Press kit</a>
+  </div>
+  <div>
+    <span class="footer-title">Legal</span>
+    <a class="link link-hover">Terms of use</a>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Footer from "$lib/components/Footer.svelte";
   import Drawer from "$lib/components/drawer/Drawer.svelte";
   import Navbar from "$lib/components/navbar.svelte";
   import ToastList from "$lib/components/toastList.svelte";
@@ -10,9 +11,11 @@
     <Navbar />
   </header>
 
-  <main class="my-4 max-w-7xl mx-auto">
+  <main class="my-8 max-w-7xl mx-auto sm:px-4 md:px-8">
     <slot />
   </main>
+
+  <Footer />
 </Drawer>
 
 <ToastList />
