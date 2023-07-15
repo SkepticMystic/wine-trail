7cf69ec04e5c5d8344f2235fe4554314d4e56e5e
SkepticMystic <rossk29@gmail.com>
2023-07-15 12:36:41 +0200

    Add Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,10 @@
   import { onMount } from "svelte";
   import "../app.css";
   import Footer from "$lib/components/Footer.svelte";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 
   let loading = true;
   onMount(async () => {
