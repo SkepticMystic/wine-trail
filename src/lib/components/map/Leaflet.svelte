137239a3a48b0940f226abfb9d894612dfe22b5f
SkepticMystic <rossk29@gmail.com>
2023-07-16 11:31:15 +0200

    Change map provider

--- src/lib/components/map/Leaflet.svelte
+++ src/lib/components/map/Leaflet.svelte
@@ -22,14 +22,11 @@
     //@ts-expect-error
     L
       //
-      .tileLayer(
-        "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
-        {
-          maxZoom: 20,
-          attribution:
-            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        }
-      )
+      .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      })
       .addTo(map);
 
     //@ts-expect-error
